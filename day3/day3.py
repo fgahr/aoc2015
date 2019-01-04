4d9877eb375f0f4ee050633715bfd7b97637e7df
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-01-05 00:38:21 +0100

    Day 3: Add solution for part two.

--- day3/day3.py
+++ day3/day3.py
@@ -6,7 +6,17 @@ from typing import NewType
 Coord = NewType('coordinate', (int, int))
 
 def part_one(data: str) -> int:
+    """Determine the number of houses receiving at least one present from Santa."""
     distinct_places = {place for place in places(data)}
+    return len(distinct_places)
+
+def part_two(data: str) -> int:
+    """Determine the number of houses receiving at least one present
+    from either Santa or Robot Santa."""
+    santa_instructions, robo_instructions = split_instructions(data)
+    santa_places = places(santa_instructions)
+    robo_places = places(robo_instructions)
+    distinct_places = {place for place in santa_places + robo_places}
     return len(distinct_places)
 
 def places(data: str) -> List[Coord]:
@@ -40,6 +50,21 @@ def move_right(p: Coord) -> Coord:
     x, y = p
     return (x + 1, y)
 
+def split_instructions(data: str) -> (str, str):
+    """Split the combined instruction set.
+
+    Return the instructions for Santa and Robo Santa."""
+    remaining = data
+    santa_instructions = ''
+    robo_instructions = ''
+    while len(remaining) > 1:
+        santa, robo, remaining = remaining[0], remaining[1], remaining[2:]
+        santa_instructions = santa_instructions + santa
+        robo_instructions = robo_instructions + robo
+    if len(remaining) > 0:
+        santa_instructions = santa_instructions + remaining
+    return santa_instructions, robo_instructions
+
 def read_data() -> str:
     with open('input.txt') as input:
         return input.readline()
@@ -47,6 +72,7 @@ def read_data() -> str:
 def main():
     data = read_data()
     print('Part one solution: {}'.format(part_one(data)))
+    print('Part two solution: {}'.format(part_two(data)))
 
 if __name__ == '__main__':
     main()
--- day3/test_day3.py
+++ day3/test_day3.py
@@ -14,5 +14,14 @@ class TestDayThree(unittest.TestCase):
     def test_p1_twitch(self):
         self.assertEqual(day3.part_one('^v^v^v^v^v'), 2)
 
+    def test_p2_up_down(self):
+        self.assertEqual(day3.part_two('^v'), 3)
+
+    def test_p2_round_trips(self):
+        self.assertEqual(day3.part_two('^>v<'), 3)
+
+    def test_p2_linear(self):
+        self.assertEqual(day3.part_two('^v^v^v^v^v'), 11)
+
 if __name__ == '__main__':
     unittest.main()

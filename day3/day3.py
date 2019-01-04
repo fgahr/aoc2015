6d98dea663224e23a87ee64d02898aedca138fa0
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-01-05 00:20:48 +0100

    Day 3: Add solution part one.

--- day3/day3.py
+++ day3/day3.py
@@ -1,7 +1,44 @@
 #!/usr/bin/env python
 
+from typing import List
+from typing import NewType
+
+Coord = NewType('coordinate', (int, int))
+
 def part_one(data: str) -> int:
-    return 1
+    distinct_places = {place for place in places(data)}
+    return len(distinct_places)
+
+def places(data: str) -> List[Coord]:
+    current_position = (0, 0)
+    positions = [current_position]
+    for instruction in data:
+        if instruction == '^':
+            current_position = move_up(current_position)
+        elif instruction == 'v':
+            current_position = move_down(current_position)
+        elif instruction == '<':
+            current_position = move_left(current_position)
+        elif instruction == '>':
+            current_position = move_right(current_position)
+        positions.append(current_position)
+    return positions
+
+def move_up(p: Coord) -> Coord:
+    x, y = p
+    return (x, y + 1)
+
+def move_down(p: Coord) -> Coord:
+    x, y = p
+    return (x, y - 1)
+
+def move_left(p: Coord) -> Coord:
+    x, y = p
+    return (x - 1, y)
+
+def move_right(p: Coord) -> Coord:
+    x, y = p
+    return (x + 1, y)
 
 def read_data() -> str:
     with open('input.txt') as input:
@@ -9,7 +46,7 @@ def read_data() -> str:
 
 def main():
     data = read_data()
-    part_one(data)
+    print('Part one solution: {}'.format(part_one(data)))
 
 if __name__ == '__main__':
     main()

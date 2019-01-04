366b71f564483c5dd1aa2fb2e925437eb87d8e86
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-01-04 21:30:13 +0100

    Day 1: Add solution for part 2, refactor to cleanly include both parts.

--- day1/day1.py
+++ day1/day1.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python
 
-def main():
+data = None
+
+def part_one():
+    """Find the resulting floor from the given elevator directions."""
+    floor = data.count('(') - data.count(')')
+    print('Part one solution: {}'.format(floor))
+
+def part_two():
+    """Find the character position (starting at 1) when the basement is entered first."""
+    position = basement_position()
+    print('Part two solution: {}'.format(position))
+
+def basement_position():
+    floor = 0
+    position = 1
+    for char in data:
+        floor = move_elevator(floor, char)
+        if floor == -1:
+            return position
+        position += 1
+    raise Exception('Basement is never entered.')
+
+def move_elevator(floor, char):
+    if char == '(':
+        return floor + 1
+    if char == ')':
+        return floor - 1
+
+def read_data():
+    global data
     with open('input.txt') as input:
         data = input.readline()
-        floor = data.count('(') - data.count(')')
-        print(floor)
+
+def main():
+    read_data()
+    part_one()
+    part_two()
 
 if __name__ == "__main__":
     main()

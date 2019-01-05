c30b44d9d547f5505d2535c4c6390a488168808b
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-01-05 01:13:34 +0100

    Day 4: Refactor part one, add part two solution.

--- day4/day4.py
+++ day4/day4.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python
 
 import hashlib
+from typing import Callable
 
 def part_one(data: str) -> int:
     """Determine the smallest number leading to a suitable md5 hash for data."""
+    return smallest_number_satisfying(data, starts_with_five_zeros)
+
+def part_two(data: str) -> int:
+    """Determine the smallest number leading to a suitable md5 hash for data."""
+    return smallest_number_satisfying(data, starts_with_six_zeros)
+
+def smallest_number_satisfying(data: str, pred: Callable[[str], bool]) -> int:
+    "The smallest number generating a hash from data which satisfies the given predicate."
     num = 1
     while True:
-        res = advent_coin_hash(data, num)
+        res = advent_coin_hash(data, num, pred)
         if res:
             return num
         num += 1
 
-def advent_coin_hash(data: str, num: int) -> str:
+def advent_coin_hash(data: str, num: int, pred: Callable[[str], bool]) -> str:
+    """Return the hash from data and num if it satisfies pred, else None."""
     h = hashlib.new('md5')
     h.update((data + str(num)).encode('utf-8'))
     result = h.hexdigest()
-    if is_valid_coin_hash(result):
+    if pred(result):
         return result
     return None
 
-def is_valid_coin_hash(md5_hash: str) -> bool:
+def starts_with_five_zeros(md5_hash: str) -> bool:
     return md5_hash.startswith('00000')
+
+def starts_with_six_zeros(md5_hash: str) -> bool:
+    return md5_hash.startswith('000000')
 
 def read_data() -> str:
     with open('input.txt') as input:
@@ -29,6 +42,7 @@ def read_data() -> str:
 def main():
     data = read_data()
     print('Part one solution: {}'.format(part_one(data)))
+    print('Part two solution: {}'.format(part_two(data)))
 
 if __name__ == '__main__':
     main()

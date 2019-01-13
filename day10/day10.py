d5fba5f5f2882db189ec8c42591d077bfd31110d
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-01-13 15:41:04 +0100

    Day 10: Add solution part two.

--- day10/day10.py
+++ day10/day10.py
@@ -7,11 +7,21 @@ from itertools import groupby, chain
 def part_one(data: str) -> int:
     """The length of the resulting string after applying
     look_and_say repeatedly 40 times."""
-    digits = list(map(int, data))
-    for _ in range(0, 40):
+    return len(iterate_look_and_say(data, 40))
+
+
+def part_two(data: str) -> int:
+    """The length of the resulting string after applying
+    look_and_say repeatedly 50 times."""
+    return len(iterate_look_and_say(data, 50))
+
+
+def iterate_look_and_say(input: str, iterations: int) -> str:
+    digits = list(map(int, input))
+    for _ in range(0, iterations):
         digits = look_and_say(digits)
 
-    return len(digits)
+    return digits
 
 
 def look_and_say(input: List[int]) -> List[int]:
@@ -31,6 +41,7 @@ def read_data() -> str:
 def main():
     data = read_data()
     print('Part one solution: {}'.format(part_one(data)))
+    print('Part two solution: {}'.format(part_two(data)))
 
 
 if __name__ == '__main__':

d3530d47f31cc1d9ea900813c085bf6143b4e330
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-01-13 02:00:38 +0100

    Day 9: Refactor to allow second part to be added easily.

--- day9/day9.py
+++ day9/day9.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from typing import Callable
+from typing import Mapping
+from typing import List
 import re
 import itertools
 
@@ -8,9 +11,11 @@ input_pattern = re.compile(r'(?P<src>\w+) to (?P<dest>\w+) = (?P<dist>\d+)')
 
 def part_one(data: str) -> int:
     """Determine the shortest path given distances from data."""
+    return examine_routes(data, min)
+
+def examine_routes(data: str, selector: Callable[[int, int], int]) -> int:
     places = []
     distances = {}
-    min_distance = 0
     for line in data.splitlines():
         source, destination, dist = parse_line(line)
         if source not in places:
@@ -25,19 +30,23 @@ def part_one(data: str) -> int:
         distances[source][destination] = dist
         distances[destination][source] = dist
         # Make sure we have a reasonable (high) starting point later.
-        min_distance += dist
 
+    # Distance of route as encountered in input; starting point for optimization.
+    extremal_distance = route_distance(places, distances)
     for route in list(itertools.permutations(places)):
-        route_distance = 0
-        previous = None
-        for current in route:
-            if not previous:
-                previous = current
-                continue
-            route_distance += distances[previous][current]
+        extremal_distance = selector(extremal_distance, route_distance(route, distances))
+    return extremal_distance
+
+def route_distance(route: List[str], distances: Mapping[str, Mapping[str, int]]) -> int:
+    route_distance = 0
+    previous = None
+    for current in route:
+        if not previous:
             previous = current
-        min_distance = min(min_distance, route_distance)
-    return min_distance
+            continue
+        route_distance += distances[previous][current]
+        previous = current
+    return route_distance
 
 
 def parse_line(line: str) -> (str, str, int):

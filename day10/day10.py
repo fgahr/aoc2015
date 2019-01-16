f3e19405f344357df3bd08f33204f5b02c83b637
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-01-16 16:29:55 +0100

    Day 10: Add doc comments.

--- day10/day10.py
+++ day10/day10.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Day 10: Elves Look, Elves Say -- Advent of Code 2015"""
 
 from typing import Iterable, List, Tuple
 from itertools import groupby, chain
@@ -17,6 +18,7 @@ def part_two(data: str) -> int:
 
 
 def iterate_look_and_say(sequence: str, iterations: int) -> str:
+    """Iterate look_and_say a given number of times starting from sequence."""
     digits = list(map(int, sequence))
     for _ in range(0, iterations):
         digits = look_and_say(digits)
@@ -25,20 +27,24 @@ def iterate_look_and_say(sequence: str, iterations: int) -> str:
 
 
 def look_and_say(sequence: List[int]) -> List[int]:
+    """Perform look-and-say on the sequence."""
     return list(chain.from_iterable(map(encode_group, groupby(sequence))))
 
 
 def encode_group(group: Tuple[int, Iterable[int]]) -> List[int]:
+    """Encode a run of identical elements as [length, element]."""
     key, elements = group
     return [len(list(elements)), key]
 
 
 def read_data() -> str:
+    """Read the data from the input file."""
     with open('input.txt') as input_file:
         return input_file.readline().rstrip()
 
 
 def main():
+    """Solve the day 10 puzzles."""
     data = read_data()
     print('Part one solution: {}'.format(part_one(data)))
     print('Part two solution: {}'.format(part_two(data)))

db1bbd413e14fdf87d331e4d22bd4eed8c712eb9
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-01-16 15:46:18 +0100

    Day 7: Code cleanup.
    One linter warning regarding too many return statements in a single function
    remains. It is not easy to get rid of and the actual code was deemed readable
    enough as it follows a strict and readily understandable pattern.

--- day7/day7.py
+++ day7/day7.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
+"""Day 7: Some Assembly Required -- Advent of Code 2015"""
 
 import re
 from typing import Callable
 from typing import Mapping
 from collections import namedtuple
 
-value_pattern = re.compile(r'(?P<value>\d+) -> (?P<out>\w+)')
-direct_pattern = re.compile(r'(?P<in>\w+) -> (?P<out>\w+)')
-not_pattern = re.compile(r'NOT (?P<in>\w+) -> (?P<out>\w+)')
-lshift_pattern = re.compile(
-    r'(?P<in>\w+) LSHIFT (?P<shift>\d+) -> (?P<out>\w+)')
-rshift_pattern = re.compile(
-    r'(?P<in>\w+) RSHIFT (?P<shift>\d+) -> (?P<out>\w+)')
+VALUE = re.compile(r'(?P<value>\d+) -> (?P<out>\w+)')
+DIRECT = re.compile(r'(?P<in>\w+) -> (?P<out>\w+)')
+NOT = re.compile(r'NOT (?P<in>\w+) -> (?P<out>\w+)')
+LSHIFT = re.compile(r'(?P<in>\w+) LSHIFT (?P<shift>\d+) -> (?P<out>\w+)')
+RSHIFT = re.compile(r'(?P<in>\w+) RSHIFT (?P<shift>\d+) -> (?P<out>\w+)')
 
 # NOTE: In the input this solution is based upon, AND was sometimes against
 # a fixed value. In those cases, the fixed value was always the left argument.
 # The same was not encountered for OR; however, it was still included here.
 # If fixed values appear as the right argument for your input, this solution
 # requires adjustments to accomodate.
-fixed_and_pattern = re.compile(r'(?P<fix>\d+) AND (?P<in>\w+) -> (?P<out>\w+)')
-and_pattern = re.compile(r'(?P<in1>\w+) AND (?P<in2>\w+) -> (?P<out>\w+)')
-fixed_or_pattern = re.compile(r'(?P<fix>\d+) OR (?P<in>\w+) -> (?P<out>\w+)')
-or_pattern = re.compile(r'(?P<in1>\w+) OR (?P<in2>\w+) -> (?P<out>\w+)')
+FIXED_AND = re.compile(r'(?P<fix>\d+) AND (?P<in>\w+) -> (?P<out>\w+)')
+AND = re.compile(r'(?P<in1>\w+) AND (?P<in2>\w+) -> (?P<out>\w+)')
+FIXED_OR = re.compile(r'(?P<fix>\d+) OR (?P<in>\w+) -> (?P<out>\w+)')
+OR = re.compile(r'(?P<in1>\w+) OR (?P<in2>\w+) -> (?P<out>\w+)')
 
 Wire = namedtuple('Wire', 'func prereq value', defaults=[None, [], None])
 
@@ -43,6 +42,7 @@ def part_two(data: str) -> int:
 
 
 def read_wires(data: str) -> Mapping[int, Wire]:
+    """Read the wiring information from data."""
     wires = {}
     for line in data.splitlines():
         wire_name, wire = get_wire(line)
@@ -52,58 +52,64 @@ def read_wires(data: str) -> Mapping[int, Wire]:
 
 def get_wire(line: str) -> (str, Wire):
     """Generate a Wire from an prereq line."""
-    m = value_pattern.fullmatch(line)
-    if m:
-        return m.group('out'), Wire(value=int(m.group('value')))
+    match = VALUE.fullmatch(line)
+    if match:
+        return match.group('out'), Wire(value=int(match.group('value')))
 
-    m = direct_pattern.fullmatch(line)
-    if m:
-        return m.group('out'), Wire(
-            func=signal_direct(), prereq=[m.group('in')])
+    match = DIRECT.fullmatch(line)
+    if match:
+        return match.group('out'), Wire(
+            func=signal_direct(), prereq=[match.group('in')])
 
-    m = not_pattern.fullmatch(line)
-    if m:
-        return m.group('out'), Wire(func=signal_not(), prereq=[m.group('in')])
+    match = NOT.fullmatch(line)
+    if match:
+        return match.group('out'), Wire(
+            func=signal_not(), prereq=[match.group('in')])
 
-    m = lshift_pattern.fullmatch(line)
-    if m:
-        return m.group('out'), Wire(
-            func=signal_lshift(int(m.group('shift'))), prereq=[m.group('in')])
+    match = LSHIFT.fullmatch(line)
+    if match:
+        return match.group('out'), Wire(
+            func=signal_lshift(int(match.group('shift'))),
+            prereq=[match.group('in')])
 
-    m = rshift_pattern.fullmatch(line)
-    if m:
-        return m.group('out'), Wire(
-            func=signal_rshift(int(m.group('shift'))), prereq=[m.group('in')])
+    match = RSHIFT.fullmatch(line)
+    if match:
+        return match.group('out'), Wire(
+            func=signal_rshift(int(match.group('shift'))),
+            prereq=[match.group('in')])
 
-    m = fixed_and_pattern.fullmatch(line)
-    if m:
-        return m.group('out'), Wire(
-            func=signal_fixed_and(int(m.group('fix'))), prereq=[m.group('in')])
+    match = FIXED_AND.fullmatch(line)
+    if match:
+        return match.group('out'), Wire(
+            func=signal_fixed_and(int(match.group('fix'))),
+            prereq=[match.group('in')])
 
-    m = and_pattern.fullmatch(line)
-    if m:
-        return m.group('out'), Wire(
-            func=signal_and(), prereq=[m.group('in1'),
-                                       m.group('in2')])
+    match = AND.fullmatch(line)
+    if match:
+        return match.group('out'), Wire(
+            func=signal_and(), prereq=[match.group('in1'),
+                                       match.group('in2')])
 
-    m = fixed_or_pattern.fullmatch(line)
-    if m:
-        return m.group('out'), Wire(
-            func=signal_fixed_or(int(m.group('fix'))), prereq=[m.group('in')])
+    match = FIXED_OR.fullmatch(line)
+    if match:
+        return match.group('out'), Wire(
+            func=signal_fixed_or(int(match.group('fix'))),
+            prereq=[match.group('in')])
 
-    m = or_pattern.fullmatch(line)
-    if m:
-        return m.group('out'), Wire(
-            func=signal_or(), prereq=[m.group('in1'),
-                                      m.group('in2')])
+    match = OR.fullmatch(line)
+    if match:
+        return match.group('out'), Wire(
+            func=signal_or(), prereq=[match.group('in1'),
+                                      match.group('in2')])
 
     raise ValueError('Line not matched: {}'.format(line))
 
 
 def determine_current(wire_name: str,
                       wires: Mapping[str, Wire]) -> (int, Mapping[str, Wire]):
+    """Determine the current on one of the given wires."""
     def get_current(name: str) -> int:
-        """Recursively calculate the current on wires as needed, memoize results."""
+        """Recursively calculate the current on wires as needed."""
         wire = wires[name]
         if not wire:
             raise LookupError('No wire found with name: {}'.format(name))
@@ -127,55 +133,72 @@ def determine_current(wire_name: str,
             wires[name] = Wire(value=value)
             return value
 
+        raise ValueError(
+            'Unhandled number of arguments for wire: {}'.format(wire))
+
     return get_current(wire_name)
 
 
 def signal_direct() -> Callable[[int], int]:
+    """A circuit no-op, transmitting the signal as-is."""
     return lambda x: x
 
 
 def signal_lshift(shift: int) -> Callable[[int], int]:
+    """A circuit element performing a fixed LSHIFT on a signal."""
     return lambda x: limit_signal(unsigned(x) << shift)
 
 
 def signal_rshift(shift: int) -> Callable[[int], int]:
+    """A circuit element performing a fixed RSHIFT on a signal."""
     return lambda x: limit_signal(unsigned(x) >> shift)
 
 
 def signal_not() -> Callable[[int], int]:
+    """A circuit element performing bitwise NOT on a signal."""
     return lambda x: limit_signal(~unsigned(x))
 
 
 def signal_and() -> Callable[[int, int], int]:
+    """A circuit element performing bitwise AND on two signals."""
     return lambda x, y: limit_signal(unsigned(x) & unsigned(y))
 
 
 def signal_fixed_and(value: int) -> Callable[[int], int]:
+    """A circuit element performing bitwise AND with a fixed value."""
     return lambda x: signal_and()(x, value)
 
 
 def signal_or() -> Callable[[int, int], int]:
+    """A circuit element performing a bitwise OR on two signals."""
     return lambda x, y: limit_signal(unsigned(x) | unsigned(y))
 
 
 def signal_fixed_or(value: int) -> Callable[[int], int]:
+    """A circuit element performing a bitwise OR with a fixed value."""
     return lambda x: signal_or()(x, value)
 
 
 def unsigned(signal: int) -> int:
+    """Convert an integer to the bit representation of an unsigned int.
+
+    Changes the value. Subsequent processing must be aware of this."""
     return signal + (1 << 32)
 
 
 def limit_signal(signal: int) -> int:
+    """Limit the signal to a value in the possible range."""
     return max(0, signal % 65536)
 
 
 def read_data() -> str:
-    with open('input.txt') as input:
-        return input.read()
+    """Read the data from the input file."""
+    with open('input.txt') as input_file:
+        return input_file.read.read()
 
 
 def main():
+    """Solve the day 7 puzzles."""
     data = read_data()
     print('Part one solution: {}'.format(part_one(data)))
     print('Part two solution: {}'.format(part_two(data)))

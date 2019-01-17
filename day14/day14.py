126dc01b9d05d4b9220b0d66152fb6dd884d2c3d
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-01-17 02:45:33 +0100

    Day 14: Improve testability, add part test and solution.

--- day14/day14.py
+++ day14/day14.py
@@ -3,30 +3,50 @@
 
 import re
 from collections import namedtuple
+from itertools import repeat
 
 INPUT = re.compile(r'(?P<name>\w+) can fly (?P<speed>\d+) km/s for ' +
                    r'(?P<fly_duration>\d+) seconds, but then must rest for ' +
                    r'(?P<rest_time>\d+) seconds.')
 
-Reindeer = namedtuple('Reindeer', 'speed fly_duration rest_time')
+Reindeer = namedtuple('Reindeer', 'name speed fly_duration rest_time')
 
 
-def part_one(data: str) -> int:
+def part_one(data: str, travel_time=2503) -> int:
     """The distance the fastest reindeer could traverse in 2503 seconds."""
-    travel_time = 2503
     distance = lambda r_deer: distance_traveled(r_deer, travel_time)
     return max(map(distance, map(reindeer_from_line, data.splitlines())))
 
+def part_two(data: str, travel_time=2503) -> int:
+    """The total score of the winner after 2503 seconds."""
+    reindeers = list(map(reindeer_from_line, data.splitlines()))
+    names = [r.name for r in reindeers]
+    scores = dict(zip(names, repeat(0, len(names))))
+    leaders = []
+    for time in range(1, travel_time + 1):
+        max_distance = 0
+        for deer in reindeers:
+            distance = distance_traveled(deer, time)
+            if distance > max_distance:
+                max_distance = distance
+                leaders = [deer.name]
+            elif distance == max_distance:
+                leaders.append(deer.name)
+        for leader in leaders:
+            scores[leader] += 1
+
+    return max(scores.values())
 
 def reindeer_from_line(line: str) -> Reindeer:
     """A reindeer as described in an input line."""
     match = INPUT.fullmatch(line)
     if not match:
         raise ValueError('Invalid input line: {}'.format(line))
+    name = match.group('name')
     speed = int(match.group('speed'))
     fly_duration = int(match.group('fly_duration'))
     rest_time = int(match.group('rest_time'))
-    return Reindeer(speed, fly_duration, rest_time)
+    return Reindeer(name, speed, fly_duration, rest_time)
 
 
 def distance_traveled(reindeer: Reindeer, time: int) -> int:
@@ -53,6 +73,7 @@ def main():
     """Solve the day 14 puzzles."""
     data = read_data()
     print('Part one solution: {}'.format(part_one(data)))
+    print('Part two solution: {}'.format(part_two(data)))
 
 
 if __name__ == '__main__':
--- day14/test_day14.py
+++ day14/test_day14.py
@@ -5,15 +5,15 @@ import day14
 
 
 class TestDayFourteen(unittest.TestCase):
-    def test_p1_comet(self):
-        data = 'Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.'
-        reindeer = day14.reindeer_from_line(data)
-        self.assertEqual(day14.distance_traveled(reindeer, 1000), 1120)
+    def test_p1_example(self):
+        data = """Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."""
+        self.assertEqual(day14.part_one(data, travel_time=1000), 1120)
 
-    def test_p1_dancer(self):
-        data = 'Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.'
-        reindeer = day14.reindeer_from_line(data)
-        self.assertEqual(day14.distance_traveled(reindeer, 1000), 1056)
+    def test_p2_example(self):
+        data = """Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."""
+        self.assertEqual(day14.part_two(data, travel_time=1000), 689)
 
 
 if __name__ == '__main__':

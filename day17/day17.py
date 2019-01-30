131fc5bee7f474d7b71664ab6c44e08150e05a59
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-01-30 14:02:59 +0100

    Day 17: Add part two test and solution.

--- day17/day17.py
+++ day17/day17.py
@@ -7,26 +7,61 @@ from typing import List
 def part_one(data: str, liters_eggnog=150) -> int:
     """The number of ways to store the given amount of eggnog in the
     containers specified in data."""
-    # Containers from data, sorted descending by capacity.
-    containers = list(map(int, data.splitlines()))
-    # Sort containers descending. This is not necessary but makes the solution
-    # more intuitive.
-    containers.sort(key=lambda x: -x)
-    return ways_to_fill(liters_eggnog, containers)
+    return ways_to_fill(liters_eggnog, get_containers(data))
+
+
+def part_two(data: str, liters_eggnog=150) -> int:
+    """The number of ways to store the given amount of eggnog TWICE."""
+    containers = get_containers(data)
+    for num_containers in range(1, len(containers) + 1):
+        ways = ways_to_fill_using_containers(liters_eggnog, num_containers,
+                                             containers)
+        if ways > 1:
+            return ways
+
+    raise AssertionError('No solution found.')
+
 
 def ways_to_fill(amount: int, containers: List[int]) -> int:
     """The number of ways to pour amount of liquid into containers.
 
     All containers used must be filled up entirely."""
-    # Reached an acceptable configuration.
     if amount == 0:
+        # Reached an acceptable configuration.
         return 1
-    # No longer satisfiable.
     if amount < 0 or not containers:
+        # No longer satisfiable.
         return 0
     # Either the first container is used or it is skipped.
     return ways_to_fill(amount - containers[0], containers[1:]) + ways_to_fill(
         amount, containers[1:])
+
+
+def ways_to_fill_using_containers(amount: int, num_containers,
+                                  containers: List[int]) -> int:
+    """The number of ways to fill amount of liquid into exactly num_containers
+    of the given available containers."""
+    if amount == 0 and num_containers == 0:
+        # Reached an acceptable configuration.
+        return 1
+    if amount < 0 or not containers or num_containers < 0:
+        # No longer satisfiable.
+        return 0
+    with_first = ways_to_fill_using_containers(
+        amount - containers[0], num_containers - 1, containers[1:])
+    without_first = ways_to_fill_using_containers(amount, num_containers,
+                                                  containers[1:])
+    return with_first + without_first
+
+
+def get_containers(data: str) -> List[int]:
+    """Read the containers from data."""
+    # Containers from data, sorted descending by capacity.
+    containers = list(map(int, data.splitlines()))
+    # Sort containers descending. This is not necessary but makes the solution
+    # more intuitive.
+    containers.sort(key=lambda x: -x)
+    return containers
 
 
 def read_data() -> str:
@@ -39,6 +74,7 @@ def main():
     """Solve the day 16 puzzles."""
     data = read_data()
     print('Part one solution: {}'.format(part_one(data)))
+    print('Part two solution: {}'.format(part_two(data)))
 
 
 if __name__ == '__main__':
--- day17/test_day17.py
+++ day17/test_day17.py
@@ -7,6 +7,10 @@ class TestDaySeventeen(unittest.TestCase):
     def test_p1_example(self):
         self.assertEqual(day17.ways_to_fill(25, [20, 15, 10, 5, 5]), 4)
 
+    def test_p2_example(self):
+        data = '\n'.join(map(str, [20, 15, 10, 5, 5]))
+        self.assertEqual(day17.part_two(data, liters_eggnog=25), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
